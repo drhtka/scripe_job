bc3b14f307c62093fa93c846591b17f16585c7c9
drhtka <drhtka@gmil.com>
2021-04-18 11:50:49 +0300

    url_dict 58

--- run-scraping.py
+++ run-scraping.py
@@ -8,42 +8,80 @@ import os, sys
 from bs4 import BeautifulSoup as BS
 from random import randint
 
+from django.contrib.auth import get_user_model
 from django.db import DatabaseError
 
-proj = os.path.dirname(os.path.abspath('manage.py'))# абсолютный путь 8 мин 51 less
-sys.path.append(proj) # добавить в системные переменные путей
-os.environ["DJANGO_SETTINGS_MODULE"] = 'scripe_job.settings' # будет установлена в переменных окружения, указываем название проекта где файл settings.py
+proj = os.path.dirname(os.path.abspath('manage.py'))  # абсолютный путь 8 мин 51 less
+sys.path.append(proj)  # добавить в системные переменные путей
+os.environ[
+    "DJANGO_SETTINGS_MODULE"] = 'scripe_job.settings'  # будет установлена в переменных окружения, указываем название проекта где файл settings.py
 
 import django
+
 django.setup()
 # 52 less scripe_job подкл django settings end
-from scraping.parsers import * # импрортим все
-from scraping.models import Vacancy, City, Language, Error
+from scraping.parsers import *  # импрортим все
+from scraping.models import Vacancy, City, Language, Error, Url
 
-parsers = (
-    (work, 'https://www.work.ua/ru/jobs-kyiv-python'),
-    (dou, 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D0%B5%D0%B2&category=Python'),
-    (djinni, 'https://djinni.co/jobs/?location=%D0%9A%D0%B8%D0%B5%D0%B2&primary_keyword=Python'),
-    (rabota, 'https://rabota.ua/zapros/python/%D1%83%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0'),
+User = get_user_model()  # вернет того пользователя который опрделён в рамках нашего джанго проекта
+#
+# parsers = (
+#     (work, 'https://www.work.ua/ru/jobs-kyiv-python'),
+#     (dou, 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D0%B5%D0%B2&category=Python'),
+#     (djinni, 'https://djinni.co/jobs/?location=%D0%9A%D0%B8%D0%B5%D0%B2&primary_keyword=Python'),
+#     (rabota, 'https://rabota.ua/zapros/python/%D1%83%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0'),
+# )
+
+parsers = ( # будем подставлят урл из словаря
+    (work, 'work'),
+    (dou, 'dou'),
+    (djinni, 'djinni'),
+    (rabota, 'rabota'),
 )
 
-city = City.objects.filter(slug='kiev').first()
-language = Language.objects.filter(slug='python').first()
+def get_settings():
+    qs = User.objects.filter(send_email=True).values()# т.е мы используем не MyUser, User того ког определила джанго
+    settings_lst = set((q['city_id'], q['language_id']) for q in qs)# получим айдишники из кверисет city_id'], q['language_id
+    # print(settings_lst)
+    return settings_lst
+
+def get_urls(_settings): # на вход будем получать данные из get_settings()
+    # нам необходимо получтиь те наборы из базы которые мы будем использовать
+    qs = Url.objects.all().values()
+    url_dict = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}# преобразовываем qs в словарь позволит нам создать набор урлов для дальнейшего испоьзования в функции
+    urls = []
+
+    for pair in _settings: # _settings  {(1, 1)}
+        tmp = {}
+        tmp['city'] = pair[0] #'city': 1,
+        tmp['language'] = pair[1] # 'language': 1,
+        # url_data = url_dict.get(pair)
+        tmp['url_data'] = url_dict[pair] # {'work': 'https://www.work.ua/ru/jobs-kyiv-python', 'rabota': 'https://rabota.ua/zapros/python/%D1%83%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0', 'dou': 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D0%B5%D0%B2&category=Python', 'djinni': 'https://djinni.co/jobs/?location=%D0%9A%D0%B8%D0%B5%D0%B2&primary_keyword=Python'}
+        urls.append(tmp) # [{'city': 1, 'language': 1, 'url_data': {'work': 'https://www.work.ua/ru/jobs-kyiv-python', 'rabota': 'https://rabota.ua/zapros/python/%D1%83%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0', 'dou': 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D0%B5%D0%B2&category=Python', 'djinni': 'https://djinni.co/jobs/?location=%D0%9A%D0%B8%D0%B5%D0%B2&primary_keyword=Python'}}]
+    return urls
+
+settings = get_settings()
+url_list = get_urls(settings)
+
+# city = City.objects.filter(slug='kiev').first()
+# language = Language.objects.filter(slug='python').first()
 
 # print(city)
 # print(language)
 jobs, errors = [], []
-for func, url in parsers:
-    # print(func, url)
-    j, e = func(url)
-    jobs += j
-    errors += e
+for data in url_list:
 
+    for func, key in parsers:
+        # print(func, url)
+        url = data['url_data'][key]
+        j, e = func(url, city=data['city'], language=data['language'])
+        jobs += j
+        errors += e
 
 for job in jobs:
-    v = Vacancy(**job, city=city, language=language)# раскрываем словарь, фильтруем по city language выборки
+    v = Vacancy(**job)  # раскрываем словарь,
     try:
-        v.save()# и записвыаем в базу данных
+        v.save()  # и записвыаем в базу данных
     except DatabaseError:
         pass
 
--- scraping/parsers.py
+++ scraping/parsers.py
@@ -15,131 +15,139 @@ headers = [
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
     ]
 
-def work(url):
+def work(url, city=None, language=None):
     jobs = []
     errors = []
     domain = 'https://www.work.ua'
-    url = 'https://www.work.ua/ru/jobs-kyiv-python/'
-    resp = requests.get(url, headers=headers[randint(0, 2)]) # делаем запрос представишись браузером рендомно [randint(0, 2)] и что то вернет в ответ
-    if resp.status_code == 200:
-        soup = BS(resp.content, 'html.parser')# что парсим метод парсера html.parser
-        main_div = soup.find('div', id='pjax-job-list')
-        if main_div:
-            div_lst = main_div.find_all('div', attrs={'class': 'job-link'})
-            for div in div_lst:
-                    title = div.find('h2')
-                    href = title.a['href'] # добираемся до  урл
-                    content = div.p.text
-                    company = 'No name'
-                    logo = div.find('img')
-                    if logo:# если мы находим img
-                        company = logo['alt']# обращаемся к атрибуту алт
-                    jobs.append({'title': title.text, 'url': domain + href,
-                                'description': content, 'company': company})
-        else:
-            errors.append({'url': url, 'title': "Table does not exists"})
-    else:
-        errors.append({'url': url, 'title': "Page is empty"})
-
-    return jobs, errors
-
-def rabota(url):
-    jobs = []
-    errors = []
-    domain = 'https://rabota.ua'
     # url = 'https://www.work.ua/ru/jobs-kyiv-python/'
-    resp = requests.get(url, headers=headers[randint(0, 2)]) # делаем запрос представишись брайзером и что то вернет в отвер
-    if resp.status_code == 200:
-        soup = BS(resp.content, 'html.parser')# что парсим метод парсера html.parser
-        new_jobs = soup.find('div',
-                                 attrs={'class': 'f-vacancylist-newnotfound'})
-        if not new_jobs: # если нет такого класса запускаем всю логику
-            table = soup.find('table', id='ctl00_content_vacancyList_gridList')
-            if table:# если таблица есть
-                tr_lst = table.find_all('tr', attrs={'id': True})# тогда ищем tr, 'id': True значит айдишник есть
-                for tr in tr_lst:
-                        div = tr.find('div', attrs={'class': 'card-body'})
-                        if div:
-                            title = div.find('h2',
-                                            attrs={'class': 'card-title'})
-                            href = title.a['href'] # добираемся до  урл
-                            content = div.p.text
-                            company = 'No name'
-                            p = div.find('p', attrs={'class': 'company-name'})
-                            if p:# если мы находим p
-                                try:
-                                    company = p.a.text
-                                except AttributeError:
-                                    print('111')
-                                # print('company')
-                                # print(p.a['title'])
-                            jobs.append({'title': title.text,
-                                         'url': domain + href,
-                                         'description': content,
-                                         'company': str(company)})
+    if url: # 58 less
+        resp = requests.get(url, headers=headers[randint(0, 2)]) # делаем запрос представишись браузером рендомно [randint(0, 2)] и что то вернет в ответ
+        if resp.status_code == 200:
+            soup = BS(resp.content, 'html.parser')# что парсим метод парсера html.parser
+            main_div = soup.find('div', id='pjax-job-list')
+            if main_div:
+                div_lst = main_div.find_all('div', attrs={'class': 'job-link'})
+                for div in div_lst:
+                        title = div.find('h2')
+                        href = title.a['href'] # добираемся до  урл
+                        content = div.p.text
+                        company = 'No name'
+                        logo = div.find('img')
+                        if logo:# если мы находим img
+                            company = logo['alt']# обращаемся к атрибуту алт
+                        jobs.append({'title': title.text, 'url': domain + href,
+                                    'description': content, 'company': company,
+                                     'city_id': city, 'language_id': language})
             else:
                 errors.append({'url': url, 'title': "Table does not exists"})
         else:
             errors.append({'url': url, 'title': "Page is empty"})
-    else:
-        errors.append({'url': url, 'title': "Page do not response"})
+
     return jobs, errors
 
-def dou(url):
+def rabota(url, city=None, language=None):
+    jobs = []
+    errors = []
+    domain = 'https://rabota.ua'
+    # url = 'https://www.work.ua/ru/jobs-kyiv-python/'
+    if url:
+        resp = requests.get(url, headers=headers[randint(0, 2)]) # делаем запрос представишись брайзером и что то вернет в отвер
+        if resp.status_code == 200:
+            soup = BS(resp.content, 'html.parser')# что парсим метод парсера html.parser
+            new_jobs = soup.find('div',
+                                     attrs={'class': 'f-vacancylist-newnotfound'})
+            if not new_jobs: # если нет такого класса запускаем всю логику
+                table = soup.find('table', id='ctl00_content_vacancyList_gridList')
+                if table:# если таблица есть
+                    tr_lst = table.find_all('tr', attrs={'id': True})# тогда ищем tr, 'id': True значит айдишник есть
+                    for tr in tr_lst:
+                            div = tr.find('div', attrs={'class': 'card-body'})
+                            if div:
+                                title = div.find('h2',
+                                                attrs={'class': 'card-title'})
+                                href = title.a['href'] # добираемся до  урл
+                                content = div.p.text
+                                company = 'No name'
+                                p = div.find('p', attrs={'class': 'company-name'})
+                                if p:# если мы находим p
+                                    try:
+                                        company = p.a.text
+                                    except AttributeError:
+                                        print('111')
+                                    # print('company')
+                                    # print(p.a['title'])
+                                jobs.append({'title': title.text,
+                                             'url': domain + href,
+                                             'description': content,
+                                             'company': str(company),
+                                             'city_id': city, 'language_id': language})
+                else:
+                    errors.append({'url': url, 'title': "Table does not exists"})
+            else:
+                errors.append({'url': url, 'title': "Page is empty"})
+        else:
+            errors.append({'url': url, 'title': "Page do not response"})
+    return jobs, errors
+
+def dou(url, city=None, language=None):
     jobs = []
     errors = []
     # domain = 'https://www.work.ua'
     # url = 'https://www.work.ua/ru/jobs-kyiv-python/'
-    resp = requests.get(url, headers=headers[randint(0, 2)]) # делаем запрос представишись брайзером и что то вернет в отвер
-    if resp.status_code == 200:
-        soup = BS(resp.content, 'html.parser')# что парсим метод парсера html.parser
-        main_div = soup.find('div', id='vacancyListId')
-        if main_div:
-            li_lst = main_div.find_all('li', attrs={'class': 'l-vacancy'})
-            for li in li_lst:
-                    title = li.find('div', attrs={'class': 'title'})
-                    href = title.a['href'] # добираемся до  урл
-                    cont = li.find('div', attrs={'class': 'sh-info'})
-                    content = cont.text
-                    company = 'No name'
-                    a = title.find('a', attrs={'class': 'company'})
-                    if a:
-                        company = a.text
-                    jobs.append({'title': title.text, 'url': href, # домен не нужен ссылка абсолютная
-                                'description': content, 'company': company})
+    if url:
+        resp = requests.get(url, headers=headers[randint(0, 2)]) # делаем запрос представишись брайзером и что то вернет в отвер
+        if resp.status_code == 200:
+            soup = BS(resp.content, 'html.parser')# что парсим метод парсера html.parser
+            main_div = soup.find('div', id='vacancyListId')
+            if main_div:
+                li_lst = main_div.find_all('li', attrs={'class': 'l-vacancy'})
+                for li in li_lst:
+                        title = li.find('div', attrs={'class': 'title'})
+                        href = title.a['href'] # добираемся до  урл
+                        cont = li.find('div', attrs={'class': 'sh-info'})
+                        content = cont.text
+                        company = 'No name'
+                        a = title.find('a', attrs={'class': 'company'})
+                        if a:
+                            company = a.text
+                        jobs.append({'title': title.text, 'url': href, # домен не нужен ссылка абсолютная
+                                    'description': content, 'company': company,
+                                     'city_id': city, 'language_id': language})
+            else:
+                errors.append({'url': url, 'title': "Table does not exists"})
         else:
-            errors.append({'url': url, 'title': "Table does not exists"})
-    else:
-        errors.append({'url': url, 'title': "Page is empty"})
+            errors.append({'url': url, 'title': "Page is empty"})
 
     return jobs, errors
 
-def djinni(url):
+def djinni(url, city=None, language=None):
     jobs = []
     errors = []
     domain = 'https://djinni.co'
     # url = 'https://www.work.ua/ru/jobs-kyiv-python/'
-    resp = requests.get(url, headers=headers[randint(0, 2)]) # делаем запрос представишись брайзером и что то вернет в отвер
-    if resp.status_code == 200:
-        soup = BS(resp.content, 'html.parser')# что парсим метод парсера html.parser
-        main_ul = soup.find('ul', attrs={'class': "list-jobs"})# заходим в ul и class
-        if main_ul: # если есть ul
-            li_lst = main_ul.find_all('li', attrs={'class': 'list-jobs__item'}) # тогда ищем все li которые имет класс list-jobs__item
-            for li in li_lst:
-                    title = li.find('div', attrs={'class': 'list-jobs__title'})# тайтл с урл
-                    href = title.a['href'] # добираемся до  урл
-                    cont = li.find('div', attrs={'class': 'list-jobs__description'})# описание
-                    content = cont.text
-                    company = 'No name'
-                    comp = li.find('div', attrs={'class': 'list-jobs__details__info'})
-                    if comp:
-                        company = comp.text # обращаемся ко всему тексту
-                    jobs.append({'title': title.text, 'url': domain + href, # домен не нужен ссылка абсолютная
-                                'description': content, 'company': company})
+    if url:
+        resp = requests.get(url, headers=headers[randint(0, 2)]) # делаем запрос представишись брайзером и что то вернет в отвер
+        if resp.status_code == 200:
+            soup = BS(resp.content, 'html.parser')# что парсим метод парсера html.parser
+            main_ul = soup.find('ul', attrs={'class': "list-jobs"})# заходим в ul и class
+            if main_ul: # если есть ul
+                li_lst = main_ul.find_all('li', attrs={'class': 'list-jobs__item'}) # тогда ищем все li которые имет класс list-jobs__item
+                for li in li_lst:
+                        title = li.find('div', attrs={'class': 'list-jobs__title'})# тайтл с урл
+                        href = title.a['href'] # добираемся до  урл
+                        cont = li.find('div', attrs={'class': 'list-jobs__description'})# описание
+                        content = cont.text
+                        company = 'No name'
+                        comp = li.find('div', attrs={'class': 'list-jobs__details__info'})
+                        if comp:
+                            company = comp.text # обращаемся ко всему тексту
+                        jobs.append({'title': title.text, 'url': domain + href, # домен не нужен ссылка абсолютная
+                                    'description': content, 'company': company,
+                                     'city_id': city, 'language_id': language})
+            else:
+                errors.append({'url': url, 'title': "Table does not exists"})
         else:
-            errors.append({'url': url, 'title': "Table does not exists"})
-    else:
-        errors.append({'url': url, 'title': "Page is empty"})
+            errors.append({'url': url, 'title': "Page is empty"})
 
     return jobs, errors
 
